251949d34ed8ca42afeb772efc83135ec336dd76
Mohammad Arif <danielharold21@gmail.com>
2022-08-25 08:22:42 +0530

    Initial commit

---
+++ app/src/main/java/com/synac/financeappprofile/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.synac.financeappprofile.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val CustomDarkGray = Color(0xFF1f1f1e)
+val CustomLightGreen = Color(0xFF1e3c3b)
+val CustomOrange = Color(0xFFe89d63)
+val CustomGreen = Color(0xFF77dc87)
+val CustomCyan = Color(0xFF6ac9da)
